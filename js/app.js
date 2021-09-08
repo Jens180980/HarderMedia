ece43d4f9abf4a404138ad7187bd62af8cbbe88a
jens180980 <jens180980@gmail.com>
2021-09-08 11:21:29 +0200

    update playground on header, insert codepens, start weather

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const tempHTML = document.getElementById('temp')
+const pluvHTML = document.getElementById('pluv')
+const pressHTML = document.getElementById('press')
+let tempData
+let pluvData
+let pressData
+
